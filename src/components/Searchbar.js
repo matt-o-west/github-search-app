11adb3f819a0ead1c3f99bc5fdd4bf62baa0299c
matt-o-west <matt.omalley.west@gmail.com>
2022-08-25 16:59:53 -0400

    completed styling of searchbar component

--- src/components/Searchbar.js
+++ src/components/Searchbar.js
@@ -1,15 +1,17 @@
 import './Searchbar.css';
 //import Followers from './Followers';
 //import Socials from './Socials';
-
+ 
 function Searchbar() {
     //const [card, setCard] = useState([]);
 
 
     return (
-        <div className="searchbar">
-            <input type="text" placeholder="Search Github username..." />
-        </div>
+        <form className="searchbar">
+            <img src="icon-search.svg" alt="search" />
+            <input className='text' type="text" placeholder="Search Github username..."/>
+            <button className='searchbutton' type="submit">Search</button>
+        </form>
     );
 }
 
