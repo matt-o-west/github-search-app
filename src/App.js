0f3be27ff2eee34afbe7e1171b7d85a3867affbd
matt-o-west <matt.omalley.west@gmail.com>
2022-08-27 15:52:19 -0400

    added initial fetch and form submit logic

--- src/App.js
+++ src/App.js
@@ -2,12 +2,28 @@ import './App.css';
 //import Topbar from './components/Topbar';
 import Searchbar from './components/Searchbar';
 import Card from './components/Card';
+import { useState, useEffect } from 'react';
 
 function App() {
+  const [user, setUser] = useState([]);
+
+  useEffect(() => {     
+    fetch(`https://api.github.com/users/${user}`)
+      .then(response => response.json())
+      .then(data => setUser(data))
+      .catch(error => console.log(error));
+}, []);
+
+  function handleFormSubmit(event) {
+    setUser(event.target.value);
+  }
+
   return (
+
+
       <header className="App-header">
-          <Searchbar />
-          <Card />
+          <Searchbar onSubmit={handleFormSubmit}/>
+          <Card user={user}/>
       </header>
   );
 }
--- src/components/Socials.js
+++ src/components/Socials.js
@@ -6,7 +6,6 @@ function Socials() {
 
     return (
         <div className="socials">
-            
             <div className="location"><img src="icon-location.svg" alt="location"/> Location</div>
             <div className="twitter"><img src="icon-twitter.svg" alt="location"/> Twitter</div>
             <div className="site"><img src="icon-website.svg" alt="location"/> Site</div>
